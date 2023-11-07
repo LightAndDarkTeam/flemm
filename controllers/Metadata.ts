8d67fbae6559511492a159f1bdb97f6a9683d209
Mickael <mickael.rassicot@gmail.com>
2023-11-07 22:13:54 +0100

    [v2] wip

--- controllers/Metadata.ts
+++ controllers/Metadata.ts
@@ -1,4 +1,7 @@
 import {Request, Response} from 'express'
+const axios = require('axios');
+import http from 'http'
+const httpAgent = new http.Agent({ keepAlive: true });
 
 import { Metadata } from "../entity/Metadata"
 
@@ -6,11 +9,55 @@ import { Metadata } from "../entity/Metadata"
 import {myDataSource}  from "../config/ormconfig"
 
 export const getMetadatas = async (req: Request, res: Response) => {
-
   const metadatas = await myDataSource
     .getRepository(Metadata)
     .createQueryBuilder('m')
     .getMany()
     
   res.json({metadatas})
+}
+
+export const updateMetadatas = async (req: Request, res: Response) => {
+  let postData = {
+    "filter": { 
+      "element": [], 
+      "rarity": [], 
+      "rank": [1, 5], 
+      "grade": [], 
+      "advancement": [], 
+      "set": ["ARKHANTE", "MANTRIS"], 
+      "tokenType": ["CARD"],
+      "animationLevel": [1] }, 
+      "pagination": { "take": 10000, "skip": 0 }
+  }
+  
+    
+  axios.post('http://localhost:5500/api/metadataaas', {...postData}, {headers: {
+    'Content-Type': 'application/json'
+  }})
+    .then(async function (response) {
+      let metadatas = response.data.cards
+
+      if (metadatas.length > 0) {
+        for (const meta of metadatas) {
+          let metadata: Metadata = new Metadata()
+
+          metadata.name = await meta.name,
+          metadata.foil = await meta.foil,
+          metadata.animationLevel = await meta.animationLevel,
+          metadata.power = await meta.power,
+          metadata.rarity = await meta.rarity,
+          metadata.rank = await meta.rank,
+          metadata.potential = await meta.potential,
+          metadata.grade = await meta.grade,
+          metadata.advancement = await meta.advancement,
+          metadata.set = await meta.set,
+          metadata.element = await meta.element,
+          metadata.cardType = await meta.cardType,
+          metadata.tokenType = await meta.tokenType
+
+          const response = await Metadata.save(metadata)
+        }
+      }
+    })
 }
--- routes/index.ts
+++ routes/index.ts
@@ -2,15 +2,17 @@ import { Router } from 'express'
 
 import {
   getMetadatas,
+  updateMetadatas
 } from '../controllers/Metadata'
 
 const router = Router();
 
 router.get('/', (req, res) => res.status(200).send({
-  message: 'Welcome to the API dude!',
+  message: 'Welcome to the API dude!'
 }));
 
 // METADATAS PART
 router.get('/metadata', getMetadatas)
+router.get('/update-metadata', updateMetadatas)
 
 export default router
